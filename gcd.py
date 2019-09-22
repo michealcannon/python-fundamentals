25683c4e9ee5473c6dd7369150ee386418e1cf54
micheal cannon <cannonmicheal@hotmail.com>
2019-09-22 22:08:52 +0100

    change gcd

--- gcd.py
+++ gcd.py
@@ -1,7 +1,13 @@
-a = 50
-b = 20
+def gcd(a,b):
 
-while b > 0:
-    a, b = b, a % b
+    """
+    returns the gcd of a and b
 
-print(a)
+    """
+
+    while b > 0:
+        a, b = b, a % b
+
+    return a
+print(gcd(20,50))
+print(gcd(120,80))
